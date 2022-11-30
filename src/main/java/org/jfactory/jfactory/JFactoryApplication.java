b6ab346915d9c112c8c63391b99775263e07aecd
abarhub <abarhub@gmail.com>
2022-11-30 19:32:36 +0100

    Initial commit

---
+++ src/main/java/org/jfactory/jfactory/JFactoryApplication.java
@@ -0,0 +1,13 @@
+package org.jfactory.jfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jfactory/jfactory/JFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jfactory.jfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
