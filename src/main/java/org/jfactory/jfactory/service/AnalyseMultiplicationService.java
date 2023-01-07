0fbd3f9e14baed86f6545b5de8cf219230a55beb
abarhub <abarhub@gmail.com>
2023-01-07 19:58:40 +0100

    début analyse

--- src/main/java/org/jfactory/jfactory/service/AnalyseMultiplicationService.java
+++ src/main/java/org/jfactory/jfactory/service/AnalyseMultiplicationService.java
@@ -1,5 +1,6 @@
 package org.jfactory.jfactory.service;
 
+import org.jfactory.jfactory.domain.Addition;
 import org.jfactory.jfactory.domain.Constante;
 import org.jfactory.jfactory.domain.Equation;
 import org.jfactory.jfactory.domain.Multiplication;
@@ -25,9 +26,149 @@ public class AnalyseMultiplicationService {
     record Param1(long a,long b, long c,long n) {}
     record Var(long x,long y) {}
 
+    @FunctionalInterface
+    interface Ajoute{
+
+        void ajoute(long multX, long multY, int constante, long valx, long valy,
+                           Addition tmp3,Equation equation,Map<Param1, Map<Var,Long>> map);
+    }
+
     public void run() {
 //        test1();
-        test2();
+//        test2();
+        test3();
+    }
+
+    public void test3() {
+        LOGGER.atInfo().log("Analyse multiplication ...");
+
+        int ordre,valeur;
+
+        ordre=2;
+        valeur=5;
+
+        try {
+            Path p = Path.of("files/double_primes_5.txt");
+            List<Mult> liste = parseFileMultiplication(p,ordre,valeur);
+
+            LOGGER.atInfo().log("liste={}",liste);
+            LOGGER.atInfo().log("size(liste)={}",liste.size());
+
+
+//            Map<Param1, Map<Var,Long>> map=listeValeursPossibles2(liste, ordre);
+            Map<Param1, Map<Var,Long>> map=listeValeursPossibles2(liste,ordre,(long multX, long multY, int constante, long valx, long valy,
+                                                Addition tmp3,Equation equation,Map<Param1, Map<Var,Long>> map2)->{
+
+                var p2 = new Param1(multX, multY, constante%10, tmp3.getValeur());
+                var v2 = new Var(valx, valy);
+
+                addMap(map2, p2, v2);
+            });
+
+            LOGGER.atInfo().log("map={}",map);
+
+            Map<Long,Long> map2=new TreeMap<>();
+            for(var tmp2:map.entrySet()){
+                for(var tmp3:tmp2.getValue().entrySet()){
+                    var n=tmp3.getValue();
+                    if(map2.containsKey(n)){
+                        map2.put(n,map2.get(n)+1);
+                    } else {
+                        map2.put(n,1L);
+                    }
+                }
+            }
+
+            LOGGER.atInfo().log("map2={}",map2);
+
+        }catch (Exception e){
+            LOGGER.atError().log("Erreur",e);
+        }
+
+    }
+
+    private Map<Param1, Map<Var, Long>> listeValeursPossibles2(List<Mult> liste, int ordre, Ajoute ajoute) {
+        Map<Param1, Map<Var,Long>> map=new HashMap<>();
+
+        for(int i = 0; i< liste.size(); i++) {
+            var tmp = liste.get(i);
+
+            MultiplicationService multiplicationService = new MultiplicationService();
+
+            var eq = multiplicationService.generationEquation(tmp.n + "");
+
+            LOGGER.atInfo().log("eq={}", eq);
+
+            affectation(eq, true, tmp.a());
+            affectation(eq, false, tmp.b);
+
+            LOGGER.atInfo().log("eq={}", eq);
+
+            var tmp2 = eq.getEquationSimplifiee(ordre);
+
+            LOGGER.atInfo().log("eq ordre 3={}", tmp2);
+
+            var x2 = eq.getVariable(true, ordre);
+            var y2 = eq.getVariable(false, ordre);
+
+            var valx2 = x2.get().getValeur();
+            var valy2 = y2.get().getValeur();
+
+            x2.get().setAffecte(false);
+            x2.get().setValeur(-1);
+            y2.get().setAffecte(false);
+            y2.get().setValeur(-1);
+
+            var tmp3 = eq.getEquationSimplifiee(ordre);
+            LOGGER.atInfo().log("eq ordre no {} : {}", ordre, tmp3);
+
+            long constante = 0;
+            long multX = -1;
+            long multY = -1;
+
+            for (var tmp4 : tmp3.getAddition()) {
+                if (tmp4 instanceof Constante cst) {
+                    constante += cst.getValeur();
+                } else if (tmp4 instanceof Multiplication m) {
+                    var v1 = m.getV1();
+                    var v2 = m.getV2();
+                    if (v1.isAffecte()) {
+                        Assert.state(!v2.isAffecte(), "v2 non affecté:" + tmp4);
+                        if (v2.isX()) {
+                            Assert.state(v2.getNom().equals(x2.get().getNom()), "v2 invalide");
+                            multX = v1.getValeur();
+                        } else {
+                            Assert.state(v2.getNom().equals(y2.get().getNom()), "v2 invalide");
+                            multY = v1.getValeur();
+                        }
+                    } else {
+                        Assert.state(v2.isAffecte(), "v2 non affecté:" + tmp4);
+                        if (v1.isX()) {
+                            Assert.state(v1.getNom().equals(x2.get().getNom()), "v1 invalide");
+                            multX = v2.getValeur();
+                        } else {
+                            Assert.state(v1.getNom().equals(y2.get().getNom()), "v1 invalide");
+                            multY = v2.getValeur();
+                        }
+                    }
+                } else {
+                    Assert.state(false, "operation non géré:" + tmp4);
+                }
+            }
+            Assert.state(multX >= 0, "mult x invalide");
+            Assert.state(multY >= 0, "mult y invalide");
+
+            LOGGER.atInfo().log("eq: {}*{} + {}*{} + {} = {}", multX, x2.get().getNom(), multY, y2.get().getNom(), constante, tmp3.getValeur());
+
+//            var p2 = new Param1(multX, multY, constante%10, tmp3.getValeur());
+//            var v2 = new Var(valx2, valy2);
+//
+//            addMap(map, p2, v2);
+            ajoute.ajoute(multX,multY,valx2,valy2,constante,tmp3,eq,map);
+
+        }
+
+        return map;
     }
 
     public void test2() {
@@ -71,7 +212,7 @@ public class AnalyseMultiplicationService {
     }
 
     private Map<Param1, Map<Var, Long>> listeValeursPossibles(List<Mult> liste, int ordre) {
-        Map<Param1, Map<Var,Long>> map=new HashMap<>();
+        /*Map<Param1, Map<Var,Long>> map=new HashMap<>();
 
         for(int i = 0; i< liste.size(); i++) {
             var tmp = liste.get(i);
@@ -150,7 +291,15 @@ public class AnalyseMultiplicationService {
 
         }
 
-        return map;
+        return map;*/
+        return listeValeursPossibles2(liste,ordre,(long multX, long multY, int constante, long valx, long valy,
+                                                   Addition tmp3,Equation equation,Map<Param1, Map<Var,Long>> map)->{
+
+            var p2 = new Param1(multX, multY, constante%10, tmp3.getValeur());
+            var v2 = new Var(valx, valy);
+
+            addMap(map, p2, v2);
+        });
     }
 
     private static void addMap(Map<Param1, Map<Var, Long>> map, Param1 p2, Var v2) {
